8475802b791bdcb6bccb23758d07b205e219627f
Márcio Lôbo <marcioloboti@gmail.com>
2018-08-22 22:30:00 -0300

    Configurando o JPA e Migrando o Banco com Flyway
    Configurando o JPA, criando o repository do objeto Ocorrencia, criando a tabela tbCarteira através da migração do Flyway

---
+++ src/main/java/com/wolf/inamax/repository/Ocorrencias.java
@@ -0,0 +1,9 @@
+package com.wolf.inamax.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wolf.inamax.model.Ocorrencia;
+
+public interface Ocorrencias extends JpaRepository<Ocorrencia, Long> {
+
+}
---
+++ src/main/resources/db/migration/V01__criar_tb_carteira.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE tbCarteira (
+    id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+    cod_negociador VARCHAR(5) NOT NULL,
+    data_acordo date NOT NULL,
+    cpf VARCHAR(16) NOT NULL,
+    cliente VARCHAR(50) NOT NULL,
+    id_cliente VARCHAR(9) NOT NULL,
+    telefone VARCHAR(12) NOT NULL,
+    saldo_devedor DECIMAL(10, 2) NOT NULL,
+    valor_acordo DECIMAL(10, 2) NOT NULL,
+    data_vencimento date NOT NULL,   
+    parcelas INT(10) NOT NULL,
+    valor_entrada DECIMAL(10, 2) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    modificadoPor VARCHAR(50) NOT NULL,
+    iniciado_em date NOT NULL,
+    finalizado_em date NOT NULL,
+    tipo_negociacao VARCHAR(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
